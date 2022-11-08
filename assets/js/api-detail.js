e3d9a37849225a298b270875c9ba1f9c6946b6b1
Abecik <afandi_army@live.com>
2022-11-08 15:20:32 +0800

    update modified

--- assets/js/api-detail.js
+++ assets/js/api-detail.js
@@ -1,9 +1,8 @@
 const detail1 = fetch("https://elestari.pendidikankelestarianjohor.edu.my/api/pemarkahan/detail")
   .then((response) => response.json())
   .then(function (data) {
-    console.log("DETAIL");
-
-    console.log(data["data"]);
+    // console.log("DETAIL");
+    // console.log(data["data"]);
   });
 detail1;
 
